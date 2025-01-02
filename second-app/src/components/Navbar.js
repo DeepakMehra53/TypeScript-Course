73468b004b8d446cbb05269997df94b50f55d505
DeepakMehra53 <dmehra594@gmail.com>
2025-01-02 10:51:46 +0530

    🎢🗃️

--- second-app/src/components/Navbar.js
+++ second-app/src/components/Navbar.js
@@ -1,54 +1,48 @@
-import React from 'react'
-
+import React from "react";
+import logo from "../images/logo.svg";
+import { pageLinks,socialLinks } from "../data";
 const Navbar = () => {
   return (
-      <nav className="navbar">
-          <div className="nav-center">
-              <div className="nav-header">
-                  <img src="./images/logo.svg" className="nav-logo" alt="backroads" />
-                  <button type="button" className="nav-toggle" id="nav-toggle">
-                      <i className="fas fa-bars"></i>
-                  </button>
-              </div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
 
-              <ul className="nav-links" id="nav-links">
-                  <li>
-                      <a href="#home" className="nav-link"> home </a>
-                  </li>
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className="nav-link">
+                  {link.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
 
-                  <li>
-                      <a href="#about" className="nav-link"> about </a>
-                  </li>
+        <ul className="nav-icons">
+            {socialLinks.map((link)=>{
+                const{id,href,icon}=link;
+                return <li key={id}>
+                    <a
+                        href={href}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="nav-icon"
+                    >
+                        <i className={icon}></i>
+                    </a>
+                </li>
+            })}
+         
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-                  <li>
-                      <a href="#services" className="nav-link"> services </a>
-                  </li>
-
-                  <li>
-                      <a href="#tours" className="nav-link"> tours</a>
-                  </li>
-              </ul>
-
-              <ul className="nav-icons">
-                  <li>
-                      <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-                      ><i className="fab fa-facebook"></i
-                      ></a>
-                  </li>
-                  <li>
-                      <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-                      ><i className="fab fa-twitter"></i
-                      ></a>
-                  </li>
-                  <li>
-                      <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-                      ><i className="fab fa-squarespace"></i
-                      ></a>
-                  </li>
-              </ul>
-          </div>
-      </nav>
-  )
-}
-
-export default Navbar
+export default Navbar;
